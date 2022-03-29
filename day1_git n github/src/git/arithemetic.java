9c92db566b337c96eb836402af05d6f3e57f9c61
DELL <DELL@DESKTOP-00IENG9>
2022-03-29 21:54:18 +0530

    first commit

---
+++ day1_git n github/src/git/arithemetic.java
@@ -0,0 +1,17 @@
+package git;
+
+public class arithemetic {
+
+	public static void main(String[] args) {
+		int a = 60;
+	      int b = 12;
+	     
+
+	      System.out.println("a + b = " + (a + b) );
+	      System.out.println("a - b = " + (a - b) );
+	      System.out.println("a * b = " + (a * b) );
+	      System.out.println("a / b = " + (a / b) );
+	      System.out.println("a % b = " + (a % b) );
+	}
+
+}

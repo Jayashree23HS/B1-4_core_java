a3bc4ff6ae73b11831ce905b778435d7eb839eec
DELL <DELL@DESKTOP-00IENG9>
2022-04-01 18:27:59 +0530

    first commit

--- day3/src/oops/car.java
+++ day3/src/oops/car.java
@@ -1,16 +1,16 @@
 package oops;
 
 public class car {
-
+	
 	void display()
 	{
-	   System.out.println("welcome to C2TC program");
+		System.out.println("welcome to c2tc program");
 	}
 
 	public static void main(String[] args) {
 		car c=new car();
 		c.display();
-		
+		// TODO Auto-generated method stub
 
 	}
 

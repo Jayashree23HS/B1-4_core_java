2de5e78fbe12c9c71671c3f59437c92d4491834a
DELL <DELL@DESKTOP-00IENG9>
2022-04-11 22:47:44 +0530

    first commit

---
+++ day10_array/src/array/Example6.java
@@ -0,0 +1,13 @@
+package array;
+
+public class Example6 {
+
+	public static void main(String[] args) {
+		//two dimensional array
+		int arr[][]= {{11,22,33},{10,20,30}};
+		System.out.println(arr[1][1]);
+		System.out.println(arr[0][2]);
+
+	}
+
+}

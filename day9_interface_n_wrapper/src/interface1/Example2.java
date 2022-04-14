fd0e3e6eebf83befa49b0afe2fe8f089a3504cfa
DELL <DELL@DESKTOP-00IENG9>
2022-04-14 22:39:52 +0530

    first commit

---
+++ day9_interface_n_wrapper/src/interface1/Example2.java
@@ -0,0 +1,27 @@
+package interface1;
+
+public interface Example2 {
+	void display();
+	void print();
+	
+}
+
+	class Main1 implements Example2
+	{
+		public void display() 
+		 {
+			 System.out.println(" mysore");
+		 }
+		public void print()
+		{
+			System.out.println("karnataka");
+		}
+		 public static void main(String args[])
+		 {
+			 Main1 m=new Main1();
+			 m.display();
+			 m.print();
+			
+		}
+
+}

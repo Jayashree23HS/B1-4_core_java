75c4cc18382a11105e7a0dd705009eb53779f8c5
DELL <DELL@DESKTOP-00IENG9>
2022-04-27 18:59:53 +0530

    first commit

---
+++ day18_java_generics/src/generics/Generic_Class.java
@@ -0,0 +1,16 @@
+package generics;
+
+public class Generic_Class<I>{
+	I val;	
+	public static void main(String[] args) {
+		
+		Generic_Class<Character>obj=new Generic_Class<>();
+		obj.val='z';
+		obj.println("");
+	}
+	public void println(String string) {
+		
+		
+	}
+
+}
